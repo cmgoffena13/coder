ac1297712644f28c38a6c392ef8c1be4ede7d6ce
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2026-04-17 01:32:58 -0600

    removing redundant function

--- src/logging_conf.py
+++ src/logging_conf.py
@@ -5,16 +5,6 @@ import structlog
 from src.settings import config
 
 
-def set_log_level(level: str) -> None:
-    """Update the logging level dynamically after setup_logging has been called."""
-
-    src_logger = logging.getLogger("src")
-    src_logger.setLevel(level)
-
-    for handler in src_logger.handlers:
-        handler.setLevel(level)
-
-
 def setup_logging() -> None:
     """Setup the logging configuration."""
     level: int = getattr(logging, config.SELENE_LOG_LEVEL)
